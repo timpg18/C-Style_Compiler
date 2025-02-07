1a90fd12678336522232689cef215b9528a500a6
pratyaksh <bhalla.pratyaksh@gmail.com>
2025-02-07 17:59:09 +0530

    updated changes

--- LEX_SOURCE/testing/test6.c
+++ LEX_SOURCE/testing/test6.c
@@ -4,10 +4,18 @@
                        INVALID STRING LITERALS 
 ===================================================================
 */
+'\z'
+'\n'
+'\\n'
+'\\b'
 '
 'hello'
-"hello;\
+"hello;
 hello"
 "hello"
 '\t\n'
-
+/* 
+adsadmalkdams dandalkd ca
+*/
+/*
+int x = 5;
