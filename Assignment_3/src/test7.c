7c11853c4b4af8997848f190f59e6d105243cd79
pratyaksh <bhalla.pratyaksh@gmail.com>
2025-04-06 23:38:05 +0530

    D

--- Assignment_3/src/test7.c
+++ Assignment_3/src/test7.c
@@ -1,12 +1,20 @@
-//ADVANCE features
+//advance features
 
-class myclass{
-public:{
-    int x,y;
-    
+int func_ellipses(int a, int b , ...){
+    return a +b;
 }
-};
+int** add(int x){
+    int a = 10;
+    return &(&a);
+}
 
 int main(){
+    //int a = func_ellipses(7,2); ERROR!
+    int a = func_ellipses(7,2,3,5,6,5,4,3,3,8,3); 
+    a = func_ellipses(8,2,5);
+    int** (*func)(int x) = &add;
+
+    int *p =  &a;
+    (*p) = 100;
 
 }
