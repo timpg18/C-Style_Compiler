8b501982f01465369063ad9f6df820ef67cc832d
timpg18 <paritgupta8@gmail.com>
2025-04-19 15:58:29 +0530

    loops not working if-else is fine

--- Assignment_4/intermediate_representation/irgen1.tac
+++ Assignment_4/intermediate_representation/irgen1.tac
@@ -1,10 +1,18 @@
 label main:
 	func_begin main
-		t#block1 = 1
-		$0 = t#block1
-		t#block1 = t#block1 + 1
-		$1 = t#block1
-		t#block1 = t#block1 + 1
+		sum#block1 = 0
+		i#block2 = 1
+	label @L0:
+		$0 = i#block2 <= 10
+		if $0 goto @L1
+		goto @L2
+	label @L1:
+		$2 = sum#block1 + i#block2
+		sum#block1 = $2
+		$1 = i#block2 + 1
+		i#block2 = $1
+		goto @L0
+	label @L2:
 		
-		return t#block1
+		return sum#block1
 	func_end main
--- Assignment_4/main.asm
+++ Assignment_4/main.asm
@@ -4,14 +4,23 @@ global main
 main:
 push rbp
 mov rbp, rsp
-mov DWORD [rbp - 4], 1
-mov eax, DWORD [rbp - 4]
-mov ecx, DWORD [rbp - 4]
-mov ecx, ecx
-add ecx, 1
+mov DWORD [rbp - 4], 0
+mov DWORD [rbp - 8], 1
+@L0:
+mov eax, DWORD [rbp - 8]
+cmp eax, 10
+jle @L1
+jmp @L2
+@L1:
+mov edx, DWORD [rbp - 4]
+mov ecx, edx
+add ecx, eax
 mov edx, ecx
-mov ecx, ecx
-add ecx, 1
-mov eax, ecx
+mov esi, eax
+add esi, 1
+mov eax, esi
+jmp @L0
+@L2:
+mov eax, edx
 pop rbp
 ret
